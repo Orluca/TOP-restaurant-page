5395a5c3111df7a3c316798d45496a1c7d115c7d
Orluca <67704819+Orluca@users.noreply.github.com>
2022-08-12 17:01:26 +0200

    basic home section

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { header } from "./modules/header.js";
 import { footer } from "./modules/footer.js";
+import { home } from "./modules/home";
 
 function buildWebsite() {
   const content = document.querySelector(".content");
   content.appendChild(header());
+  content.appendChild(home());
   content.appendChild(footer());
 }
 
--- src/modules/home.js
+++ src/modules/home.js
@@ -1,9 +1,25 @@
+function welcomeHeadline() {
+  const headline = document.createElement("h2");
+  headline.textContent = "Welcome!";
+
+  return headline;
+}
+
+function welcomeText() {
+  const welcomeText = document.createElement("p");
+  welcomeText.textContent =
+    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+
+  return welcomeText;
+}
+
 function home() {
-  const element = document.createElement("h1");
+  const home = document.createElement("div");
 
-  element.textContent = "Restaurant Name";
+  home.appendChild(welcomeHeadline());
+  home.appendChild(welcomeText());
 
-  return element;
+  return home;
 }
 
 export { home };
