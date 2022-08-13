f470509f59ba381780e746a484fb9cc0d26ed891
Orluca <67704819+Orluca@users.noreply.github.com>
2022-08-13 14:24:28 +0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import { header } from "./modules/header.js";
 import { footer } from "./modules/footer.js";
-import { home } from "./modules/home.js";
-import { menu } from "./modules/menu";
-import { contact } from "./modules/contact";
 
 function main() {
   const main = document.createElement("div");
@@ -11,22 +8,11 @@ function main() {
   return main;
 }
 
-function changeContent() {
-  //   const main = main();
-  //   main.innerHTML = "";
-  //   main.appendChild(home());
-}
-
 function buildWebsite() {
   const content = document.querySelector(".content");
   content.appendChild(header());
-  //   content.appendChild(home());
-  //   content.appendChild(contact());
-  //   content.appendChild(menu());
   content.appendChild(main());
   content.appendChild(footer());
-
-  changeContent();
 }
 
 buildWebsite();
